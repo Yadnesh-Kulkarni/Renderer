20cfee065b0cad6f8e66aa008b0b59d2158241ee
Yadnesh Kulkarni <yadneshdkulkarni@gmail.com>
2026-06-16 08:25:38 -0400

    Add taskflow integration

--- Engine/src/core/Engine.cpp
+++ Engine/src/core/Engine.cpp
@@ -1,3 +1,5 @@
+#include <taskflow/taskflow/taskflow.hpp>
+#include <taskflow/taskflow/algorithm/for_each.hpp>
 #include "core/Engine.h"
 #include "renderer/Vulkan/core/GEVulkan.h"
 
@@ -14,6 +16,8 @@ EngineCore::~EngineCore()
 
 void EngineCore::Run()
 {
+    TestTaskFlow();
+
     int width = 1280;
 	int height = 720;
     window->createWindow(width, height, "Vulkan window");
@@ -29,4 +33,27 @@ void EngineCore::Run()
     }
 
     renderer->Cleanup();
+}
+
+void EngineCore::TestTaskFlow()
+{
+	tf::Taskflow taskflow;
+    std::vector<int> data = {0, 1 , 2 , 3, 4, 5, 6, 7, 8};
+  
+	auto task = taskflow.for_each_index(
+    0u, uint32_t(data.size()), 1u, [&](int i) {
+			printf("%d ", data[i]);
+    }).name("for_each_index");
+	
+    taskflow.emplace([]() {
+    printf("\nS - Start\n"); }).name("S").precede(task);
+    taskflow.emplace([]() {
+    printf("\nT - End\n"); }).name("T").succeed(task);
+
+    std::ofstream os(".cache/taskflow.dot");
+    taskflow.dump(os);
+
+
+    tf::Executor executor;
+	executor.run(taskflow).wait();
 }
